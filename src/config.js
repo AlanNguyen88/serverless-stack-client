4fd90a2f937ff513c94a12f62f5b127253e8fa66
Minh Nguyen <Minh@titaniumrhino.com>
2019-12-03 15:51:51 -0500

    Setting up our React App

--- src/config.js
+++ src/config.js
@@ -1,16 +1,16 @@
 export default {
     s3: {
-      REGION: us-east-1,
-      BUCKET: skyenotes
+      REGION: "us-east-1",
+      BUCKET: "skyenotes"
     },
     apiGateway: {
-      REGION: us-east-1,
-      URL: https://clz4c31kt1.execute-api.us-east-1.amazonaws.com/prod
+      REGION: "us-east-1",
+      URL: "https://clz4c31kt1.execute-api.us-east-1.amazonaws.com/prod"
     },
     cognito: {
-      REGION: us-east-1,
-      USER_POOL_ID: us-east-1_4C9NJBppE,
-      APP_CLIENT_ID: 21q39970a5atl9r59jsoui4ktb,
-      IDENTITY_POOL_ID: us-east-1:76b84289-1e14-4829-81e6-d784eb7d41e4
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_4C9NJBppE",
+      APP_CLIENT_ID: "21q39970a5atl9r59jsoui4ktb",
+      IDENTITY_POOL_ID: "us-east-1:76b84289-1e14-4829-81e6-d784eb7d41e4"
     }
   };
